2087e19d26176b7b57cc01ea52dd70138a4db8a7
Marvin Ahlgrimm <m.j.ahlgrimm@gmx.de>
2013-01-17 16:40:13 +0100

    Added friendlist, options to add/remove friends

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,13 +1,17 @@
 class FriendshipsController < ApplicationController
   def index
-    
+    @friendships = User.find_by_id(params[:user_id]).friendships
   end
   
   def create
-    
+    @friend = User.find(params[:user_id])
+    current_user.befriend!(@friend)
+    redirect_to :back, notice: "Added #{@friend.full_name} to your friendlist."
   end
   
   def destroy
-    
+    @friend = User.find(params[:user_id])
+    current_user.unfriend!(@friend)
+    redirect_to :back, notice: "Removed #{@friend.full_name} from your friendlist."
   end
 end
--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,2 +1,5 @@
 module FriendshipsHelper
+  def friend(friendship)
+    User.find_by_id(friendship.other_friend_id)
+  end
 end
